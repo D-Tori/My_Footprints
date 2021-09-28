5188e8b60d0f5e6bbb3ae32b78020e70eb1c5215
giseok <hgsok3056@naver.com>
2021-09-29 00:51:46 +0900

    09/29 [기능추가] - eslint / prettier 설치

--- front/src/test.tsx
+++ front/src/test.tsx
@@ -2,10 +2,11 @@ import React from 'react';
 import './test.scss';
 
 const App = () => {
-  return <div className='test_container'>
-          <h1>hello react123</h1>
-         </div>
+  return (
+    <div className="test_container">
+      <h1>hello react123</h1>
+    </div>
+  );
 };
 
 export default App;
-
