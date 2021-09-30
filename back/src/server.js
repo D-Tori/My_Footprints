128cacae20b9b5e2edcac6b840b8f4bfa9b13c2b
fepanbr <fepanbr@gmail.com>
2021-09-30 23:09:06 +0900

    [기능추가] jest 설정

---
+++ back/src/server.js
@@ -0,0 +1,10 @@
+const app = require("./app");
+
+require("dotenv").config();
+
+const { PORT } = process.env;
+
+app.listen(PORT, () => {
+  /* eslint-disable-next-line */
+  console.log(`listening to server at ${PORT}`);
+});
