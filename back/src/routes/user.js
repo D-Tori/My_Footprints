cdea6bd50c5c43e8e6042c1c4c8f6e91d0b5d873
fepanbr <fepanbr@gmail.com>
2021-10-08 00:54:05 +0900

    테스트 코드 작성해보기

--- back/src/routes/user.js
+++ back/src/routes/user.js
@@ -7,8 +7,24 @@ const db = require("../db");
 
 module.exports = router;
 
+const users = [
+  {
+    id: 1,
+    name: "박철현",
+  },
+];
+
 router.get("/:id", async (req, res) => {
   const { id } = req.params;
-  const { rows } = await db.query("SELECT * FROM users WHERE id = $1", [id]);
-  res.send(rows[0]);
+
+  const findUser = users.find((user) => user.id.toString() === id);
+  if (findUser) {
+    res.status(200).send(JSON.stringify(findUser));
+  } else {
+    res.status(404).end();
+  }
+
+  // const { id } = req.params;
+  // const { rows } = await db.query("SELECT * FROM users WHERE id = $1", [id]);
+  // res.send(rows[0]);
 });
