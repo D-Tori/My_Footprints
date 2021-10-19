64efaa2a87c0a25ce352a22e5f2bb8bd6dc2ea55
giseok <hgsok3056@naver.com>
2021-10-19 21:17:02 +0900

    10/19 styled-component 설치

--- front/src/test.tsx
+++ front/src/test.tsx
@@ -1,11 +1,29 @@
 import React from 'react';
-import './test.scss';
+// import './test.scss';
+
+import styled from 'styled-components';
+
+const Teststyle = styled.div`
+  width: 500px;
+  height: 200px;
+  margin: 0 auto;
+  background-color: yellow;
+`;
+
+// const 변수명 = (매개변수명:자료형) => {처리구문}
+// const App = () => {
+//   return (
+//     <div className="test_container">
+//       <h1>hello react123</h1>
+//     </div>
+//   );
+// };
 
 const App = () => {
   return (
-    <div className="test_container">
+    <Teststyle>
       <h1>hello react123</h1>
-    </div>
+    </Teststyle>
   );
 };
 
